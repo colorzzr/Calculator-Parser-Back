c3cf99c20a16800e9baab2c0965acb7f82407e93
Rui Fu <freelz@gmail.com>
2018-06-13 10:47:46 +0800

    init

---
+++ controller/schema.go
@@ -0,0 +1,9 @@
+package controller
+
+func init(){
+	schemaExtendInit()
+}
+
+func schemaExtendInit() {
+
+}
---
+++ hdswitch-backend.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/freeznet/tomato"
+	"github.com/freeznet/tomato/config"
+	"fmt"
+	_ "github.com/freeznet/hdswitch-backend/controller"
+)
+
+func main() {
+	tomato.Run()
+}
+
+func initLiveQueryServer() {
+	args := map[string]string{}
+	args["pattern"] = "/livequery"
+	args["addr"] = ":8089"
+	args["logLevel"] = "VERBOSE"
+	args["serverURL"] = "http://127.0.0.1:8080/v1"
+	args["appId"] = config.TConfig.AppID
+	args["clientKey"] = config.TConfig.ClientKey
+	args["masterKey"] = config.TConfig.MasterKey
+	args["keyPairs"] = fmt.Sprintf("{\"javascriptKey\":\"%s\"}", config.TConfig.JavaScriptKey)
+	args["subType"] = "EventEmitter"
+	go tomato.RunLiveQueryServer(args)
+}
